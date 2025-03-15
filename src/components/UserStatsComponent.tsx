6a1cf20842d002d673cf2cc61abed5178bbab9d5
axel <ssaxel03@gmail.com>
2025-03-15 21:19:11 +0000

    style: dark mode square point border on stats

--- src/components/UserStatsComponent.tsx
+++ src/components/UserStatsComponent.tsx
@@ -19,27 +19,27 @@ export default async function Stats({
         <section className="flex flex-col gap-4 items-center justify-center w-full my-4" id="info">
 
             <div className="flex flex-row items-center justify-center w-full gap-6">
-                <div className="aspect-square h-6 dark:bg-second-dark bg-second-light rounded-md border-b border-solid border-dark"></div>
+                <div className="aspect-square h-6 dark:bg-second-dark bg-second-light rounded-md border-b border-solid dark:border-third-dark border-dark"></div>
                 <div className="w-full">{format(userStats.stars)} stars</div>
             </div>
 
             <div className="flex flex-row items-center justify-center w-full gap-6">
-                <div className="aspect-square h-6 dark:bg-second-dark bg-second-light rounded-md border-b border-solid border-dark"></div>
+                <div className="aspect-square h-6 dark:bg-second-dark bg-second-light rounded-md border-b border-solid dark:border-third-dark border-dark"></div>
                 <div className="w-full">{format(userStats.totalContributions)} contributions</div>
             </div>
 
             <div className="flex flex-row items-center justify-center w-full gap-6">
-                <div className="aspect-square h-6 dark:bg-second-dark bg-second-light rounded-md border-b border-solid border-dark"></div>
+                <div className="aspect-square h-6 dark:bg-second-dark bg-second-light rounded-md border-b border-solid dark:border-third-dark border-dark"></div>
                 <div className="w-full">{format(userStats.contributionsThisYear)} contributions this year</div>
             </div>
 
             <div className="flex flex-row items-center justify-center w-full gap-6">
-                <div className="aspect-square h-6 dark:bg-second-dark bg-second-light rounded-md border-b border-solid border-dark"></div>
+                <div className="aspect-square h-6 dark:bg-second-dark bg-second-light rounded-md border-b border-solid dark:border-third-dark border-dark"></div>
                 <div className="w-full">{format(userStats.totalCommits)} commits</div>
             </div>
 
             <div className="flex flex-row items-center justify-center w-full gap-6">
-                <div className="aspect-square h-6 dark:bg-second-dark bg-second-light rounded-md border-b border-solid border-dark"></div>
+                <div className="aspect-square h-6 dark:bg-second-dark bg-second-light rounded-md border-b border-solid dark:border-third-dark border-dark"></div>
                 <div className="w-full">{format(userStats.commitsThisYear)} commits this year</div>
             </div>
 
