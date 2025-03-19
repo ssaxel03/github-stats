3d7e35193e6ca03a36fed7a73cef72942ad5c37a
axel <ssaxel03@gmail.com>
2025-03-19 22:50:48 +0000

    fix: header login margin instead of whole section margin

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -44,14 +44,14 @@ export default function Header({
         <>
 
             {!(profile.login == "init") && (
-                <section className="flex flex-col items-center justify-center w-full px-2 my-4">
+                <section className="flex flex-col items-center justify-center w-full px-2 my-16">
 
                     <div className="overflow-hidden mb-4 border-b-2 border-r-2 dark:border-third-dark border-solid border-dark rounded-[17px] aspect-square h-24">
                         <img className="block dark:hidden w-full h-full aspect-square" src={profile.avatar_url} alt="Profile picture" />
                         <img className="dark:block hidden w-full h-full aspect-square" src={profile.avatar_url_dark} alt="Profile picture" />
                     </div>
                     <a target={profile.html_url == "#NOT_FOUND" ? `_self` : `_blank`} href={profile.html_url}>
-                        <h1 className="text-2xl mb-16 dark:text-light hover:text-accent-orange">
+                        <h1 className="text-2xl dark:text-light hover:text-accent-orange">
                             {profile.login}
                         </h1>
                     </a>
