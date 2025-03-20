8b21ee72bc4314cbe15b7cf6ee7081caca884734
axel <ssaxel03@gmail.com>
2025-03-20 17:04:39 +0000

    fix: recent commits invalid username check

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -88,7 +88,7 @@ export async function getRecentCommits(username: string): Promise<CommitInfo[]> 
 
         const searchData = await searchResponse.json();
 
-        if (searchData.items.author.login != username) {
+        if (searchData.items[0].author.login != username) {
             return [];
         }
 
